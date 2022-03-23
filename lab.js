cc6146c2ab6fbeb5515dd0a44c283f5d3bf87e41
Lucas Spencer <lucas.spencer14@gmail.com>
2022-03-23 15:56:16 -0600

    beginner done

--- lab.js
+++ lab.js
@@ -1,58 +1,74 @@
-let lovesCode = true
-if (lovesCode = true){
-console.log("I love to code!")
-} else {
-  console.log("Coding has it's challenges")
-};
+// let lovesCode = true
+// if (lovesCode = true){
+// console.log("I love to code!")
+// } else {
+//   console.log("Coding has it's challenges")
+// };
 
-// For problems 2-3 use the following lines of code:
-var amysAge = 34;
-var brittanisAge = 35;
-var amysBirthYear = 1991;
-var brittanisBirthYear = 1986;
+// // For problems 2-3 use the following lines of code:
+// var amysAge = 34;
+// var brittanisAge = 35;
+// var amysBirthYear = 1991;
+// var brittanisBirthYear = 1986;
 
-////////// PROBLEM 2 //////////
+// ////////// PROBLEM 2 //////////
 
-/*
-  Using an if statement and a comparison operator, create code that will check if Amy or Brittani is older, and then console.log '{name} is older'. 
-  Hint: Consider what will happen if they are the same age. Handle this situation.
-*/
+// /*
+//   Using an if statement and a comparison operator, create code that will check if Amy or Brittani is older, and then console.log '{name} is older'. 
+//   Hint: Consider what will happen if they are the same age. Handle this situation.
+// */
 
-if(amysAge > brittanisAge){
-  console.log("Amy's older")
-} else if(amysAge < brittanisAge){
-  console.log("Brittanis's older")
-  } else {
-    console.log("They're the same age")
-  };
+// if(amysAge > brittanisAge){
+//   console.log("Amy's older")
+// } else if(amysAge < brittanisAge){
+//   console.log("Brittanis's older")
+//   } else {
+//     console.log("They're the same age")
+//   };
 
-////////// PROBLEM 3 //////////
+// ////////// PROBLEM 3 //////////
 
-/*
-  Using an if statement and a comparison operator, create code that will check if Amy and Brittani were born in the same year. 
-  If they were, console.log 'Amy & Brittani were born in the same year'. If they were not, console.log 'Amy & Brittani were not born in the same year'
-*/
+// /*
+//   Using an if statement and a comparison operator, create code that will check if Amy and Brittani were born in the same year. 
+//   If they were, console.log 'Amy & Brittani were born in the same year'. If they were not, console.log 'Amy & Brittani were not born in the same year'
+// */
 
-if(amysBirthYear === brittanisBirthYear){
-  console.log("Amy & Brittani were born in the same year")
-} else {
-  console.log("Amy & Brittani were not born in the same year")
-};
+// if(amysBirthYear === brittanisBirthYear){
+//   console.log("Amy & Brittani were born in the same year")
+// } else {
+//   console.log("Amy & Brittani were not born in the same year")
+// };
 
 
 ////////// PROBLEM 4 //////////
 
-let temperature = 55;
-let rain = true;
+let temp = 59;
+let rain = false;
 
 /*
-  Using an if statement and a comparison operator, create code that will log a suggestion on what type of clothes one should wear for the day, based on the above temperature and rain.
-  If it is 80 degrees or above, and raining, one should wear a t-shirt and take an umbrella. If it is between 60 and 80 degrees, and raining, one should wear a rain-jacket. If it is 60 degrees or below, one should wear a jacket and carry an umbrella. Come up with your own clothing suggestions based on those temperature parameters when it is not raining.
+  Using an if statement and a comparison operator, create code that will log a suggestion on what type of 
+  clothes one should wear for the day, based on the above temperature and rain.
+  If it is 80 degrees or above, and raining, one should wear a t-shirt and take an umbrella. If it is 
+  between 60 and 80 degrees, and raining, one should wear a rain-jacket. If it is 60 degrees or below, 
+  one should wear a jacket and carry an umbrella. Come up with your own clothing suggestions based on 
+  those temperature parameters when it is not raining.
 */
 
-//CODE HERE
+// if (temp >= 80 && rain === true){
+// console.log("wear a t-shirt & bring umbrella")
+// } else if (temp < 80 && temp > 60 && rain === true){
+// console.log("wear a rain-jacket")
+// } else if (temp <= 60 && rain === true){
+// console.log("wear jacket & bring umbrella")
+// } else if (temp >= 80 && rain === false){
+//   console.log("wear a t-shirt")
+//   } else if (temp < 80 && temp > 60 && rain === false){
+//   console.log("wear a shirt")
+//   } else if (temp <= 60 && rain === false){
+//   console.log("wear a jacket")
+//   };
 
-////////// PROBLEM 5 //////////
+// ////////// PROBLEM 5 //////////
 
 /*
   Create a for-loop that runs 10 times, and on each iteration logs the word 'hello'.
@@ -66,15 +82,19 @@ let rain = true;
   Create a for-loop that runs 10 times, logging the numbers 1 through 10. Ex, on the first iteration it would log 1, on the second iteration it would log 2.
 */
 
-//CODE HERE
+// for(i = 1; i <= 10; i++){
+//   console.log([i])
+// };
 
-////////// PROBLEM 7 //////////
+// ////////// PROBLEM 7 //////////
 
-/*
-  Create a for-loop that logs the numbers 0 through 10 backwards. Ex, on the first iteration it would log 10, on the second iteration it would log 9.
-*/
+// /*
+//   Create a for-loop that logs the numbers 0 through 10 backwards. Ex, on the first iteration it would log 10, on the second iteration it would log 9.
+// */
 
-//CODE HERE
+// for(i = 10; i >= 0; i--){
+//   console.log([i])
+// };
 
 
 ////////// PROBLEM 8 //////////
@@ -83,10 +103,14 @@ let score = 0;
 let passingScore = 7;
 
 /*
-  Create a while-loop that logs "Your score is not high enough" while the score variable above is below passingScore. In each iteration of the loop, increase the score by one. If your code is successful, you will see your logged statement 7 times.
+  Create a while-loop that logs "Your score is not high enough" while the score variable above is below passingScore. 
+  In each iteration of the loop, increase the score by one. If your code is successful, you will see your logged statement 7 times.
 */
 
-//CODE HERE
+while(score < passingScore){
+score++
+console.log(score)
+};
 
 ////////// INTERMEDIATE PROBLEMS //////////
 
